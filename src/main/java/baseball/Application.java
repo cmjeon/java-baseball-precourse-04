4797c278f37624ffa22849445ef3f4d0f7e85f1a
cmj <chmin82@gmail.com>
2022-04-18 22:33:58 +0900

    <feat> : upload Application.java
    게임을 생성하는 기능
    - 홈팀, 어웨이팀, 심판, 스코어보드를 생성하여 게임을 생성한다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,16 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BaseballGame baseballGame = createGame();
+        baseballGame.startGame();
     }
+
+    static BaseballGame createGame() {
+        HomeTeam homeTeam = new HomeTeam();
+        AwayTeam awayTeam = new AwayTeam();
+        Umpire umpire = new Umpire();
+        ScoreBoard scoreBoard = new ScoreBoard();
+        return new BaseballGame(homeTeam, awayTeam, umpire, scoreBoard);
+    }
+
 }
